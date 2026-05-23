b0d3900a0516e47614d9a1574a53b13b295cc49e
Jorge Martinez <jormarbur@gmail.com>
2026-05-23 16:29:56 -0500

    commit actualiza

--- public/auth.php
+++ public/auth.php
@@ -3,9 +3,7 @@
 require_once(__DIR__ . '/../config/conexion.php');
 require_once(__DIR__ . '/../controllers/LoginController.php');
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+session_start(); // 
 
 $controller = new LoginController($pdo);
 
